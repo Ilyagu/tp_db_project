259306dde0c4975a8b3a0d64ea6d30ae0dae62f3
Ilyagu <inagdimaev@mail.ru>
2022-05-09 14:35:05 +0300

    lol

--- cmd/main.go
+++ cmd/main.go
@@ -29,7 +29,7 @@ func main() {
 	p.Use(router)
 
 	dbpool, err := pgxpool.Connect(context.Background(),
-		"host=localhost port=5432 user=ilyagu dbname=forum password=password sslmode=disable",
+		"host=89.208.196.139 port=5432 user=ilyagu dbname=forum password=password sslmode=disable",
 	)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
